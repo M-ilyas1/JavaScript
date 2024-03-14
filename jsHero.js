310c5ba3ba428ff6eeba52e583eb3ce83016dc29
M-ilyas1 <mohammadilyas6800@gmail.com>
2024-03-14 21:58:40 +0500

    add new file jsHero

---
+++ jsHero.js
@@ -0,0 +1,53 @@
+let x = 'Laurel';
+let y = 'Hardy';
+let z = y;
+y = x;
+x = z
+
+// console.log(x);
+
+
+// ...........................
+
+function hello() {
+    return 'Hello World';
+}
+
+
+// ...........................
+
+function a() {
+    return 'Hello a!'
+}
+
+function b() {
+    return 'Hello b!'
+}
+
+
+// ...........................
+
+function greet() {
+    return 'Haydo!'
+}
+let salutation = greet();
+
+
+// ...........................
+
+function echo(param) {
+    return param
+}
+
+// ...........................
+
+function greet(name) {
+    return 'Hello' + name;
+}
+greet(Ilyas)
+
+// ...........................
+
+function log() {
+    console.log('Hello Console!');
+}
